f6a6bff79040bb71403c6c9703a198b3f7f51817
夏飞 <xiafei571@gmail.com>
2021-05-05 23:15:21 +0900

    URL本地化

--- main/demo.go
+++ main/demo.go
@@ -6,11 +6,9 @@ import (
 	"github.com/xiafei571/blueblue/rabbitMQ"
 )
 
-const MQURL = "amqp://blue:blueblue@128.199.116.194:5672//blue"
-
 func main() {
-	config = initConfigure()
-	MQURL = "amqp://" + config.rabbitMQ.User + "+:" + config.rabbitMQ.Password + "@" + config.rabbitMQ.IP + "//" + config.rabbitMQ.Vhost
+	config := initConfigure()
+	MQURL := "amqp://" + config.rabbitMQ.User + "+:" + config.rabbitMQ.Password + "@" + config.rabbitMQ.IP + "//" + config.rabbitMQ.Vhost
 	fmt.Println(MQURL)
 	rabbitmq := rabbitMQ.NewRabbitMQSimple(MQURL, ""+"blue")
 	rabbitmq.PublishSimple("Hello World!")
